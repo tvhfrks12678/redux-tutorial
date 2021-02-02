d1941450828efcdccf1691e5af7c2eebc6c20a8b
tvhfrks12678 <tvhfrks12678@gmail.com>
2021-02-02 15:01:23 +0900

    finish Preparing Action Payloads

--- src/features/posts/AddPostForm.js
+++ src/features/posts/AddPostForm.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { useDispatch } from 'react-redux'
-import { nanoid } from '@reduxjs/toolkit'
+
 import { postAdded } from './postsSlice'
 
 export const AddPostForm = () => {
@@ -14,13 +14,7 @@ export const AddPostForm = () => {
 
   const onSavePostClicked = () => {
     if (title && content) {
-      dispatch(
-        postAdded({
-          id: nanoid(),
-          title,
-          content,
-        })
-      )
+      dispatch(postAdded(title, content))
 
       setTitle('')
       setContent('')
