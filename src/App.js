be1929ead338d6a3d4220ddb497c5d46e14a4a80
tvhfrks12678 <tvhfrks12678@gmail.com>
2021-02-02 14:42:34 +0900

    Creating an Edit Post Form

--- src/App.js
+++ src/App.js
@@ -8,8 +8,9 @@ import {
 
 import { Navbar } from './app/Navbar'
 import { AddPostForm } from './features/posts/AddPostForm'
-
 import { PostsList } from './features/posts/PostsList'
+import { EditPostForm } from './features/posts/EditPostForm'
+import { SinglePostPage } from './features/posts/SinglePostPage'
 
 function App() {
   return (
@@ -27,6 +28,8 @@ function App() {
               </React.Fragment>
             )}
           />
+          <Route exact path="/posts/:postId" component={SinglePostPage} />
+          <Route exact path="/editPost/:postId" component={EditPostForm} />
           <Redirect to="/" />
         </Switch>
       </div>
---
+++ src/features/posts/EditPostForm.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useHistory } from 'react-router-dom'
+
+import { postUpdated } from './postsSlice'
+
+export const EditPostForm = ({ match }) => {
+  const { postId } = match.params
+  const post = useSelector((state) =>
+    state.posts.find((post) => post.id === postId)
+  )
+  const [title, setTitle] = useState(post.title)
+  const [content, setContent] = useState(post.content)
+
+  const dispatch = useDispatch()
+  const history = useHistory()
+
+  const onTitleChange = (e) => setTitle(e.target.value)
+  const onContentChange = (e) => setContent(e.target.value)
+
+  const onSavePostClicked = () => {
+    if (title && content) {
+      dispatch(postUpdated({ id: postId, title, content }))
+      history.push(`/posts/${postId}`)
+    }
+  }
+
+  return (
+    <setion>
+      <h2>Edit Post</h2>
+      <form>
+        <label htmlFor="postTitle">Post Title:</label>
+        <input
+          type="text"
+          id="postTitle"
+          name="postTitle"
+          placeholder="What's on your mind?"
+          value={title}
+          onChange={onTitleChange}
+        />
+        <label htmlFor="postContent">Content:</label>
+        <textarea
+          id="postContent"
+          name="postContent"
+          value={content}
+          onChange={onContentChange}
+        />
+      </form>
+      <button type="button" onClick={onSavePostClicked}>
+        Save Post
+      </button>
+    </setion>
+  )
+}
